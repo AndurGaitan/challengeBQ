573c727c29570eb349acbc0dab823a4f8ecf0bc9
AndurGaitan <andresdavidagaitan@hotmail.com>
2023-11-20 20:09:59 -0300

    agregando views

--- challenge10.1/src/app.js
+++ challenge10.1/src/app.js
@@ -9,6 +9,10 @@ import session from 'express-session'
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/public'))
+
 
 app.use(session({
     secret: 'secre6t',
@@ -19,8 +23,66 @@ app.use(session({
 initializePassport()
 app.use(passport.initialize())
 
+// Configurar handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
 app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
 app.use('/session', sessionRouter)
+app.use(viewRouter)
 
-app.listen(config.appPort, () => console.log('Servidor corrientdo en el puerto 8080'))
+const httpServer = app.listen(port, ()=> console.log(`Server Up ${port}`));
+const io = new Server(httpServer)
+
+// Configurar socket del lado del servidor
+io.on('connection', async(socket)=> {
+    console.log(`Cliente nuevo conectado ${socket.id}`)
+
+// recibe el producto y lo guarda (mongo)
+    socket.on('new-product', async(newProduct)=> {
+        try {
+            const newProductCreated = await productMongo.addNewProducts(newProduct)
+            io.emit('product-created', newProductCreated)
+        } catch (error) {
+            console.error(`Error al crear el producto ${error}`)
+        }
+    })
+
+// Recibe el id del producto que quiere eliminar (mongo)
+    socket.on('deleteProduct', async(productId)=> {
+        try {
+        await productMongo.deleteProduct(productId)
+        io.emit('deleting-product', productId)
+        } catch (error) {
+            console.error(`Error al eliminar el producto ${error}`)
+        }
+    })
+    
+
+    // obtiene los mensjaes
+    try {
+        const messageData2 =  await messageManager.getAllMessagesChat()
+        io.emit('messagesLogs', messageData2)
+    } catch(error) {
+        console.error('Error al obtener los mensajes')
+    }
+
+    // obtiene los mensajes nuevos de los clientes
+    socket.on('message', async(data)=> {
+        const {user, message} = data
+        try {
+            await messageManager.addNewMessage(user, message)
+            const messageData = await messageManager.getAllMessagesChat()
+            io.emit('messagesLogs', messageData)
+        } catch (error) {
+            console.error('Error al guardar el mensaje')
+        }
+    })
+    
+    
+    io.emit('mensajeGeneral', 'Este es un mensaje para todos')
+})
+
+
