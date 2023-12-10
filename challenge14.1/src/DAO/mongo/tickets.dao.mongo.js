c7a72112d88180f6e4caf5d8de6ca0a397e52acc
AndurGaitan <andresdavidagaitan@hotmail.com>
2023-12-10 18:49:30 -0300

    agregando documentacion swagger

---
+++ challenge14.1/src/DAO/mongo/tickets.dao.mongo.js
@@ -0,0 +1,51 @@
+import TicketModel from './models/tickets.model.js';
+
+export default class Ticket {
+  create = async(ticketData) =>{
+    try {
+      const newTicket = new TicketModel(ticketData);
+      return await newTicket.save();
+    } catch (error) {
+      throw error;
+    }
+
+  }
+  
+  get = async() => {
+    try {
+      return await TicketModel.find();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  getById = async(code) => {
+    try {
+      return await TicketModel.findById(code);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  update = async(code, ticketData) => {
+    try {
+      return await TicketModel.findByIdAndDelete( code , ticketData, { new: true });
+    } catch (error) {
+      throw error;
+    }
+
+  }
+
+  delete = async(code) => {
+    try {
+      return await TicketModel.findByIdAndDelete(code);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+}
+
+ 
+
+
